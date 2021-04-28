7d5c9a8c2dfc320c34fed8309e4d93a36b4f15ab
Moffat Sehudi <msehudi@gmail.com>
2021-04-27 22:31:22 -0700

    Update

--- cms-portal/Controllers/WeatherForecastController.cs
+++ cms-portal/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace .Controllers
+namespace cms_portal.Controllers
 {
     [ApiController]
     [Route("[controller]")]
