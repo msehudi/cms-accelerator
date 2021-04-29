55990a464e56b253f79ea5b8680e4c783ca325fc
Moffat Sehudi <msehudi@gmail.com>
2021-04-28 22:57:50 -0700

    Change dotnet version to one that the openshift sandbox can host

--- try-create/Controllers/WeatherForecastController.cs
+++ try-create/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace cms_portal.Controllers
+namespace try_create.Controllers
 {
     [ApiController]
     [Route("[controller]")]
